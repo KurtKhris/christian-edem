4c79f2917d71113fbfcbb3c2fd5c39c6899d8608
KurtKhris <christiankpegah@gmail.com>
2023-04-27 11:30:39 +0000

    vercel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
 import { Home } from "./screens/Home";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="container-fluid">
       <Home/>
+      <Analytics/>
     </div>
   );
 }
